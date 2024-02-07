d78dc7443e812d0a4d23af01b33aab95a93c4934
PeterHUistyping <2447276617@qq.com>
2024-02-07 13:52:48 +0000

    React structure change to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
+import book from './asset/book.jpg';
+import music from './asset/music.jpg';
+import movie from './asset/movie.jpg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Movies x Books x Music Recommendation System!
+        </p>
+       
+        <img src={book} alt="book" height="200" ></img>
+        <img src={music} alt="music" height="180" ></img>
+        <img src={movie} alt="movie" height="160" ></img>
+
+        Dev Notice: 
+        Change me from src/index.js <br/>
+        Reference: <a href="https://reactjs.org">Learn React</a> <br/>
+
+      </header>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
     <App />
+
   </React.StrictMode>
 );
 
