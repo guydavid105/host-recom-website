b60e564acaaeaf6ff52c24d01a021a53d5d9b200
PeterHUistyping <2447276617@qq.com>
2024-02-07 19:41:30 +0000

    header changed

--- src/js/helper/Top.js
+++ src/js/helper/Top.js
@@ -2,7 +2,8 @@ export function Top(){
     return(
     <>
        <center>
-            <h1>
+            <h1 style={{
+            }}>
             Movies x Books x Music Recommendation System!
             </h1>
             <i>Trust me, we are <b>the Best!</b> 🥇</i> 
