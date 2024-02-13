ec7bade81ccf9df183ac12fbfec2ba65cfb48100
Robert Woodland <robertwoodland47@gmail.com>
2024-02-13 12:46:00 +0000

    Add ratings

--- letterboxd/letterboxd/spiders/letterboxd_spider.py
+++ letterboxd/letterboxd/spiders/letterboxd_spider.py
@@ -21,11 +21,26 @@ class LetterboxdSpider(scrapy.Spider):
 
         # Films
         films = response.css('li.poster-container')
+
         film_titles = []
         film_uids = []
+        film_ratings = []
         for film in films:
             film_titles.append(film.css('div.film-poster img::attr(alt)').get())
             film_uids.append(film.css('div.film-poster::attr(data-film-id)').get())
+            film_rating = film.css('p.poster-viewingdata span.rating::text').get()
+            if film_rating:
+                rating = 0
+                for char in film_rating:
+                    if char == "★":
+                        rating += 1
+                    elif char == "½":
+                        rating += 0.5
+                    else:
+                        print(f"error: rating not recognized. \n {char}")
+                film_ratings.append(rating) 
+            else:
+                film_ratings.append('')
 
         # load in JSON to update
         filename = 'film_data.json'
@@ -33,12 +48,13 @@ class LetterboxdSpider(scrapy.Spider):
             data = json.load(file) # Should get an empty list, or list of dicts
         file.close()
         # Process the extracted data
-        for title, film_uid in zip(film_titles, film_uids):
+        for title, film_uid, film_rating in zip(film_titles, film_uids, film_ratings):
             yield {
                 'title': title,
                 'uid': film_uid,
+                'rating': film_rating,
             }
-            data.append({'title': title, 'uid': film_uid})
+            data.append({'title': title, 'uid': film_uid, 'rating': film_rating})
 
         # Store the data into the JSON file
         with open(filename, 'w') as file:
