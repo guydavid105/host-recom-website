4765d13307f622927237886230c3f88be5673d22
PeterHUistyping <2447276617@qq.com>
2024-02-26 14:41:53 +0000

    update scroll with backend data

--- src/js/helper/horizontalScroll.jsx
+++ src/js/helper/horizontalScroll.jsx
@@ -1,15 +1,12 @@
 import { motion, useTransform, useScroll } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
 // import "./output.css";
 
 // using tailwind css 
 const HorizontalScroll = () => {
-  <style>
-   
-</style>
+
   return (
-  
     <div className="bg-neutral-800">
       <div className="flex h-48 items-center justify-center">
         <span className="font-semibold uppercase text-white">
@@ -36,21 +33,56 @@ const HorizontalScrollCarousel = () => {
   });
 
   const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+  const [jsonData, setJsonData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/people/recomm/test/476376617723');
+        const data = await response.json();
+        setJsonData(data);
+        // console.log(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once after the initial render
 
   return (
     <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+             <div>
+    {/* {jsonData ? (
+      <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+    ) : (
+      <p>Loading...</p>
+    )} */}
+    </div>
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
         <motion.div style={{ x }} className="flex gap-4">
-          {cards.map((card) => {
+        
+        {jsonDataFixed.map((card) => {
             return <Card card={card} key={card.id} />;
           })}
+          {/* {jsonData.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })} */}
+          {/* {jsonData ? (
+      <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+    ) : (
+       
+    )} */}
         </motion.div>
       </div>
     </section>
   );
 };
 
+
+
 const Card = ({ card }) => {
+
   return (
     <div
       key={card.id}
@@ -58,13 +90,17 @@ const Card = ({ card }) => {
     >
       <div
         style={{
-          backgroundImage: `url(${card.url})`,
+          // backgroundImage: `url(${"./asset/movie.jpg"})`,
+          backgroundColor: "black",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
         className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
-        
-      ></div>
+      >
+        <img src={card.img} 
+        alt={card.title} 
+        className="hidden"/>
+      </div>
       <div className="absolute inset-0 z-10 grid place-content-center"
            onClick={() => popup(card)}
       >
@@ -83,44 +119,46 @@ const popup = (card) => {
 
 export default HorizontalScroll;
 
+const jsonDataFixed = [{"id":706083,"type":"movie","title":"Wonka","img":"https://a.ltrbxd.com/resized/film-poster/7/0/6/0/8/3/706083-wonka-0-500-0-750-crop.jpg"},{"id":764890,"type":"movie","title":"The Iron Claw","img":"https://a.ltrbxd.com/resized/film-poster/7/6/4/8/9/0/764890-the-iron-claw-2023-0-500-0-750-crop.jpg"},{"id":724394,"type":"movie","title":"The Exorcist: Believer","img":"https://a.ltrbxd.com/resized/film-poster/7/2/4/3/9/4/724394-the-exorcist-believer-0-500-0-750-crop.jpg"},{"id":835774,"type":"movie","title":"Saltburn","img":"https://a.ltrbxd.com/resized/film-poster/8/3/5/7/7/4/835774-saltburn-0-500-0-750-crop.jpg"},{"id":301552,"type":"movie","title":"Ferrari","img":"https://a.ltrbxd.com/resized/film-poster/3/0/1/5/5/2/301552-ferrari-2023-0-500-0-750-crop.jpg"},{"id":503402,"type":"movie","title":"Mission: Impossible – Dead Reckoning Part One","img":"https://a.ltrbxd.com/resized/film-poster/5/0/3/4/0/2/503402-mission-impossible-dead-reckoning-part-one-0-500-0-750-crop.jpg"},{"id":715856,"type":"movie","title":"Beau Is Afraid","img":"https://a.ltrbxd.com/resized/film-poster/7/1/5/8/5/6/715856-beau-is-afraid-0-500-0-750-crop.jpg"},{"id":868491,"type":"movie","title":"Air","img":"https://a.ltrbxd.com/resized/film-poster/8/6/8/4/9/1/868491-air-2023-0-500-0-750-crop.jpg"},{"id":721333,"type":"movie","title":"Cocaine Bear","img":"https://a.ltrbxd.com/resized/film-poster/7/2/1/3/3/3/721333-cocaine-bear-0-500-0-750-crop.jpg"},{"id":558056,"type":"movie","title":"Knock at the Cabin","img":"https://a.ltrbxd.com/resized/film-poster/5/5/8/0/5/6/558056-knock-at-the-cabin-0-500-0-750-crop.jpg"},{"id":465649,"type":"movie","title":"M3GAN","img":"https://a.ltrbxd.com/resized/film-poster/4/6/5/6/4/9/465649-m3gan-0-500-0-750-crop.jpg"},{"id":63058,"type":"movie","title":"Avatar: The Way of Water","img":"https://a.ltrbxd.com/resized/film-poster/6/3/0/5/8/63058-avatar-the-way-of-water-0-500-0-750-crop.jpg"},{"id":782545,"type":"movie","title":"Sanctuary","img":"https://a.ltrbxd.com/resized/film-poster/7/8/2/5/4/5/782545-sanctuary-2022-0-500-0-750-crop.jpg"},{"id":908010,"type":"movie","title":"How to Blow Up a Pipeline","img":"https://a.ltrbxd.com/resized/film-poster/9/0/8/0/1/0/908010-how-to-blow-up-a-pipeline-0-500-0-750-crop.jpg"},{"id":542001,"type":"movie","title":"Bros","img":"https://a.ltrbxd.com/resized/film-poster/5/4/2/0/0/1/542001-bros-0-500-0-750-crop.jpg"}];
+
 const cards = [
   {
-    url: "asset/book.jpg",
+    img: "asset/book.jpg",
     title: "Book",
     id: 1,
   },
   {
-    url: "asset/movie.jpg",
+    img: "asset/movie.jpg",
     title: "Movie",
     id: 2,
   },
   {
-    url: "asset/music.jpg",
+    img: "asset/music.jpg",
     title: "Music",
     id: 3,
   },
   {
-    url: "asset/book.jpg",
+    img: "asset/book.jpg",
     title: "Book",
     id: 4,
   },
   {
-    url: "asset/movie.jpg",
+    img: "asset/movie.jpg",
     title: "Movie",
     id: 5,
   },
   {
-    url: "asset/music.jpg",
+    img: "asset/music.jpg",
     title: "Music",
     id: 6,
   },  
   {
-    url: "asset/book.jpg",
+    img: "asset/book.jpg",
     title: "Book",
     id: 7,
   },
   {
-    url: "asset/movie.jpg",
+    img: "asset/movie.jpg",
     title: "Movie",
     id: 8,
   }
